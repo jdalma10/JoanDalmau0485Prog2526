c183ba97f4fbaf00b8905cf8d4513992f0d0f6e2
jdalma10 <jdalma10@xtec.cat>
2025-10-07 16:57:47 +0200

    Exercicis 5- de l1 al 5.

---
+++ A3. Estructures de control/EXERCICIS 5. Estructures de control de l'1 al 18/Ex2.py
@@ -0,0 +1,13 @@
+# 2. Algorisme que demani un nombre i digui si és positiu, negatiu o 0.
+
+num1 = int(input("Primer nombre: "))
+
+if num1 > 0:
+    print("es positiu")
+
+elif num1 < 0:
+    print("negatiu")
+
+else:
+    print("es cero")
+
---
+++ A3. Estructures de control/EXERCICIS 5. Estructures de control de l'1 al 18/Ex3.py
@@ -0,0 +1,10 @@
+# 3. Escriu un programa que llegeixi un nombre i indiqui si és parell o imparell.
+num1 = int(input("Primer nombre: "))
+
+if num1 %2 == 0:
+    print("es parell")
+
+else:
+    print("es imparell")
+
+
